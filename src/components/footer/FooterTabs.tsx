e2c8cacbd5a1edd1fec62a88bcd8b2597cb1a9c3
Gabryjiel <33352685+Gabryjiel@users.noreply.github.com>
2020-12-01 23:49:35 +0100

    refactor: footerTabs to styledcomponents p2

--- src/components/footer/FooterTabs.tsx
+++ src/components/footer/FooterTabs.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 
 function FooterTabs({tabs, location}: FooterTabsProps){
     
     return(
-        <div className='footer-tabs'>
+        <FooterTabsContainer>
 
             {tabs && tabs.map((tab, idx) => {
                 
@@ -11,14 +12,14 @@ function FooterTabs({tabs, location}: FooterTabsProps){
 
                 return(
                     <Link href={tab.href} key={`Tab no ${idx}`}>
-                        <div className={`${isActive ? 'footer-tabs-active' : ''}`}>
+                        <FooterTab active={isActive}>
                             {tab.content}
-                        </div>
+                        </FooterTab>
                     </Link>
                 )}
             )}
 
-        </div>
+        </FooterTabsContainer>
     )
 }
 
@@ -28,3 +29,28 @@ type FooterTabsProps = {
     tabs: Array<any>,
     location: string
 }
+
+const FooterTabsContainer = styled.div({
+    maxWidth: "60bw",
+    display: "flex",
+    justifyContent: "space-around",
+    borderTop: "1px solid black",
+    borderLeft: "1px solid black",
+    alignItems: "center",
+    backgroundColor: "bisque"
+})
+
+const FooterTab = styled.div<{active: boolean}>(({active}: any) => ({
+    color: active ? 'red' : 'inherit',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexBasis: 0,
+    flexGrow: 1,
+    width: "10vw",
+    fontSize: "large",
+    "&:hover": {
+        cursor: active ? "inherit" : "pointer",
+        textDecoration: active ? "inherit" : "underline"
+    }
+}));
